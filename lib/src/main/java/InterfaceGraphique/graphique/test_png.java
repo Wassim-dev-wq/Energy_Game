819e33cf692ad7253a1eddb4666ec0ca099b6767
Arman <arman@ozyonum.com>
2023-03-30 07:54:19 +0200

    Update test_png.java

--- lib/src/main/java/InterfaceGraphique/graphique/test_png.java
+++ lib/src/main/java/InterfaceGraphique/graphique/test_png.java
@@ -1,16 +1,59 @@
-package InterfaceGraphique.graphique;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
 
 public class test_png extends JPanel{
+    public static List<String> sources = new ArrayList<String>();
+    public static List<List<String>> directions = new ArrayList<List<String>>();
+    public static List<String> data_list = new ArrayList<String>();
+    public static int height = 0;
+    public static int length = 0;
+    public static String format;
     public test_png(){
         setSize(500, 500);
         setBackground(Color.black);
+    }
+
+    public void readFile(String fileName){
+        try{
+            File level_file = new File("./Levels/"+fileName);
+            Scanner scanner = new Scanner(level_file);
+            while (scanner.hasNext()){
+                data_list.add(scanner.next());
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        height = Integer.parseInt(data_list.get(0));
+        length = Integer.parseInt(data_list.get(1));
+        format = data_list.get(2);
+        for (int i = 3; i<data_list.size(); i++){
+            String s = data_list.get(i);
+            if (s.equals(".") || s.equals("S") || s.equals("L")){
+                List<String> position = new ArrayList<String>();
+                if (i+1 < data_list.size()){
+                    i++;
+                    while (!(data_list.get(i).equals(".") || data_list.get(i).equals("S") || data_list.get(i).equals("L"))){
+                        position.add(data_list.get(i));
+                        i++;
+                    }
+                    sources.add(s);
+                    directions.add(position);
+                }else{
+                    sources.add(s);
+                    directions.add(position);
+                    break;
+                }
+            }
+            i--;
+        }
     }
     @Override
     public void paintComponent(Graphics g) {
@@ -64,20 +107,43 @@ public class test_png extends JPanel{
         x = 240;
         BufferedImage light_long_path = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         light_long_path.getGraphics().drawImage(src, 0, 0, w, h, x, y, x + w, y + h, null);
-        g.drawImage(empty, 0, 0, this);
-        g.drawImage(dark_curved_path, 120, 0, this);
-        g.drawImage(dark_lamp, 240, 0, this);
-        g.drawImage(energy, 0, 120, this);
-        g.drawImage(dark_curved_path, 120, 120, this);
-        g.drawImage(empty,240, 120, this);
+        readFile("level1.nrg");
+        for(int j=0; j<height; j++){
+            for (int k=0; k<length; k++){
+                System.out.println("x = " + k*120);
+                System.out.println("y = " + j*120);
+                if (sources.get(j+k+(j*height)).equals(".")){
+                    if (directions.get(j+k+(j*height)).size() == 0){
+                        g.drawImage(empty, k*120, j*120, this);
+                    }else{
+                        g.drawImage(dark_curved_path, k*120, j*120, this);
+                    }
+                } else if (sources.get(j+k+(j*height)).equals("S")) {
+                    g.drawImage(energy, k*120, j*120, this);
+                } else if (sources.get(j+k+(j*height)).equals("L")) {
+                    g.drawImage(dark_lamp, k*120, j*120, this);
+                } else if (sources.get(j+k+(j*height)).equals("W")) {
+                    g.drawImage(dark_wifi, k*120, j*120, this);
+                }
+            }
+        }
+//        g.drawImage(empty, 0, 0, this);
+//        g.drawImage(dark_curved_path, 120, 0, this);
+//        g.drawImage(dark_lamp, 240, 0, this);
+//        g.drawImage(energy, 0, 120, this);
+//        g.drawImage(dark_curved_path, 120, 120, this);
+//        g.drawImage(empty,240, 120, this);
     }
 
     public static void main(String[] args) throws IOException {
+        test_png t = new test_png();
+        t.readFile("level1.nrg");
+        System.out.println(length);
+        System.out.println(height);
+        System.out.println(sources);
+        System.out.println(directions);
         JFrame f = new JFrame();
         f.add(new test_png());
-
-//        ImageIO.write(dst, "png", new File("duke_cropped.png"));
-
         f.setSize(500, 500);
         f.setBackground(Color.BLACK);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,4 +151,3 @@ public class test_png extends JPanel{
         f.setVisible(true);
     }
 }
-
