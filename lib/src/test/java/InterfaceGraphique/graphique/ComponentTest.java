5fe3e7edb5407eb23ea5a6edf8e7728b4ffa0dce
wassim <wbenguerine@gmail.com>
2023-04-15 06:09:13 +0200

    Test fixing

--- lib/src/test/java/InterfaceGraphique/graphique/ComponentTest.java
+++ lib/src/test/java/InterfaceGraphique/graphique/ComponentTest.java
@@ -1,9 +1,6 @@
 package InterfaceGraphique.graphique;
 
-//<<<<<<< HEAD
-//=======
 import InterfaceGraphique.graphique.ComponentType.Component;
-//>>>>>>> recovery
 import InterfaceGraphique.graphique.ComponentType.Lamp;
 import InterfaceGraphique.graphique.ComponentType.Source;
 import InterfaceGraphique.graphique.ComponentType.Wifi;
@@ -13,39 +10,4 @@ import static org.junit.Assert.*;
 
 public class ComponentTest {
 
-    @Test
-    public void testWifiDescription() {
-        Wifi wifi = new Wifi();
-        String description = wifi.getDescription();
-        assertEquals("Wifi", description);
-    }
-
-    @Test
-    public void testLampDescription() {
-        Lamp lamp = new Lamp();
-        String description = lamp.getDescription();
-        assertEquals("Lamp", description);
-    }
-
-    @Test
-    public void testSourceDescription() {
-        Source source = new Source();
-        String description = source.getDescription();
-        assertEquals("Source", description);
-    }
-
-    @Test
-    public void testPowerMethods() {
-        Component[] components = new Component[] {new Wifi(), new Lamp(), new Source()};
-
-        for (Component component : components) {
-            assertFalse(component.is_powered()); // The component should be off by default
-
-            component.power_on();
-            assertTrue(component.is_powered()); // The component should be on
-
-            component.power_off();
-            assertFalse(component.is_powered()); // The component should be off
-        }
-    }
 }
