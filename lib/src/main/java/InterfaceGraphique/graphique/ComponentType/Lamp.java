96cd1edb80ee6c951e79d15c6a4ab547a6ebe66b
wassim <wbenguerine@gmail.com>
2023-05-15 06:01:19 +0200

    Rotations direction fixing

--- lib/src/main/java/InterfaceGraphique/graphique/ComponentType/Lamp.java
+++ lib/src/main/java/InterfaceGraphique/graphique/ComponentType/Lamp.java
@@ -19,23 +19,21 @@ public class Lamp extends Component {
     private int angle;
     private boolean isOn;
     private List<String> directions;
-    //TODO
+    private boolean isFirstRotation = true;
+
     @Override
     public void rotate() {
-        System.out.println("Lamp");
-        System.out.println("format -> " + gridType);
         if (gridType.equals("S")) {
-            this.angle += 90;
-            if (this.angle > 270) { // to reset the angle to 0 when it goes back to the source
+            this.angle -= 90;
+            if (this.angle <  -270) { // to reset the angle to 0 when it goes back to the source
                 this.angle = 0;
             }
         } else {
-            this.angle += 60;
-            if (this.angle > 300) {
+            this.angle -= 60;
+            if (this.angle <  -300) {
                 this.angle = 0;
             }
         }
-        System.out.println("angle_turn ->" + angle);
         updateGraphics(0, 0, 120, 120, gridType, angle );
     }
 
@@ -44,6 +42,7 @@ public class Lamp extends Component {
         this.isOn = isOn;
         this.directions = directions;
         this.gridType = gridType;
+
         updateGraphics(x, y, w, h, gridType, angle);
     }
 
@@ -54,6 +53,13 @@ public class Lamp extends Component {
             x = 240;
         }else{
             x = 600;
+        }
+        double anchory;
+        if (isFirstRotation) {
+            anchory = (double) h / 2;
+            isFirstRotation = false;
+        } else {
+            anchory = (double) h / 2.3;
         }
         lamp = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         lamp.getGraphics().drawImage(src, 0, 0, w, h, x, y, x + w, y + h, null);
@@ -66,11 +72,12 @@ public class Lamp extends Component {
         for (int i=0; i<directions.size(); i++){
             double rotationAngle;
             if(gridType.equals("S")){
-                rotationAngle = Math.toRadians(angle + (Integer.parseInt(directions.get(i)) * 90));
+                rotationAngle = Math.toRadians(angle - (Integer.parseInt(directions.get(i)) * 90));
+                darkShortPathTransform = AffineTransform.getRotateInstance(rotationAngle, (double) w / 2, (double) h / 2);
             }else {
-                rotationAngle = Math.toRadians(angle + (Integer.parseInt(directions.get(i)) * 60));
+                rotationAngle = Math.toRadians(angle - (Integer.parseInt(directions.get(i)) * 60));
+                darkShortPathTransform = AffineTransform.getRotateInstance(rotationAngle, (double) w / 2, anchory);
             }
-            darkShortPathTransform = AffineTransform.getRotateInstance(rotationAngle, (double) w / 2, (double) h / 2);
 
             g_combined_lamp_dark.drawImage(shortPath.getCurrentImage(), darkShortPathTransform, null);
         }
