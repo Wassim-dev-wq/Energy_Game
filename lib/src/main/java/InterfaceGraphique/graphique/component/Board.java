3024f19bcf039da8ddd7b893d3b6be07f132a88d
wassim <wbenguerine@gmail.com>
2023-05-20 00:28:48 +0200

    backup

--- lib/src/main/java/InterfaceGraphique/graphique/component/Board.java
+++ lib/src/main/java/InterfaceGraphique/graphique/component/Board.java
@@ -86,6 +86,11 @@ public class Board extends JPanel {
                         directionToComponentMap.put(direction, clickedComponent);
                     }
                 }
+                for (Map.Entry<String, Component> entry : directionToComponentMap.entrySet()) {
+                    String name = entry.getKey();
+                    Component component = entry.getValue();
+                    System.out.println("Name :"+name+" componenet : "+component+"\n");
+                }
                 ElectricityHandler electricityHandler = level.getElectricityHandler();
                 electricityHandler.propagateElectricity();
                 this.repaint();
